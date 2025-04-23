39abbfd047c77869498bc76b0e81c3b234bb0be1
Uzo Ugochukwu <uugochukwu@aol.com>
2025-04-23 19:35:14 +0100

    completed queries

---
+++ db/queries.sql
@@ -0,0 +1,13 @@
+\c nc_news
+
+-- select * from users
+
+-- select * from articles where topic = 'coding'
+
+-- select body from comments where votes <0
+
+-- select distinct description from topics
+
+-- select title from articles where author = 'grumpy19'
+
+select body from comments where votes > 10
