2f04ccd5122566173c0cdd3993059eeaf4a2b87a
Uzo Ugochukwu <uugochukwu@aol.com>
2025-05-02 15:27:47 +0100

    first 6 parts of hosting ticket

--- db/seeds/listen.js
+++ db/seeds/listen.js
@@ -1,5 +1,10 @@
 const app = require("../../app.js")
 
-app.listen(9090, () => {
-    console.log("Server is listening on port 9090...");
-  });
+// app.listen(9090, () => {
+//     console.log("Server is listening on port 9090...");
+//   });
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () =>
+console.log(`Listening on ${PORT}...`));
