e9339c06efafbe36fa7c4eb81b11e6ffb910e15e
Uzo Ugochukwu <uugochukwu@aol.com>
2025-05-19 08:59:02 +0100

    completed task 12

--- models/nc_news.models.js
+++ models/nc_news.models.js
@@ -1,116 +1,131 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
 exports.selectTopics = () => {
- return db.query("SELECT * FROM topics ;")
- .then(( { rows }) => {
+  return db.query("SELECT * FROM topics ;").then(({ rows }) => {
     return rows;
- })
-}
+  });
+};
 
 exports.selectArticleById = (article_id) => {
- 
-    return db
-   .query(`select * from articles where article_id = $1; `, [article_id])
-   .then((result) => {
-
-      return result.rows[0]
-   })
-
-}
+  
+  // if comment_count is available use
+  
+  return db
+    .query(`select count(second.comment_id), first.author, first.title, first.article_id, first.topic, first.created_at, first.votes, first.article_img_url, first.body
+  from articles first
+  left join comments second
+  on first.article_id = second.article_id
+  where first.article_id = $1
+  group by first.author, first.title, first.article_id, first.topic, first.created_at, first.votes, first.article_img_url, first.body;
+`, [article_id])
+    .then((result) => {
+      return result.rows[0];
+    });
+};
 
 exports.selectArticles = (sort_by, order_by, topic) => {
-
-let queryStr = `select first.author, first.title, first.article_id, 
+  let queryStr = `select first.author, first.title, first.article_id, 
    first.topic, first.created_at, first.votes, first.article_img_url,
     count(second.comment_id) as count from articles as first 
     left join comments as second on second.article_id = first.article_id 
     group by first.author, first.title, first.article_id, first.topic, 
-    first.created_at, first.votes, first.article_img_url `
- 
-let queryArgs = []
+    first.created_at, first.votes, first.article_img_url `;
 
-const promiseArray = []
+  let queryArgs = [];
 
-const validSortQueries = ["author", "title", "article_id", "topic", "created_at", "votes", "article_img_url", "count"]
+  const promiseArray = [];
 
-const validOrderQueries = ["asc", "desc"]
+  const validSortQueries = [
+    "author",
+    "title",
+    "article_id",
+    "topic",
+    "created_at",
+    "votes",
+    "article_img_url",
+    "count",
+  ];
 
-const validTopicQueries = ["mitch", "cats"]
+  const validOrderQueries = ["asc", "desc"];
 
-if(sort_by && validSortQueries.includes(sort_by)) {
-   queryStr += ` order by first.${sort_by} `
-}
+  const validTopicQueries = ["mitch", "cats"];
 
-if(order_by && validOrderQueries.includes(order_by)) {
-   queryStr += `${order_by};`
-}
+  if (sort_by && validSortQueries.includes(sort_by)) {
+    queryStr += ` order by first.${sort_by} `;
+  }
 
-if(topic && validTopicQueries.includes(topic)) {
-   
-   queryStr = `select first.author, first.title, first.article_id, 
+  if (order_by && validOrderQueries.includes(order_by)) {
+    queryStr += `${order_by};`;
+  }
+
+  if (topic && validTopicQueries.includes(topic)) {
+    queryStr = `select first.author, first.title, first.article_id, 
    first.topic, first.created_at, first.votes, first.article_img_url,
     count(second.comment_id) as count from articles as first 
     left join comments as second on second.article_id = first.article_id
     where first.topic = '${topic}' group by first.author, first.title, first.article_id, first.topic, 
-    first.created_at, first.votes, first.article_img_url ;`
-}
+    first.created_at, first.votes, first.article_img_url ;`;
+  }
 
-promiseArray.unshift(db.query(queryStr, queryArgs))
+  promiseArray.unshift(db.query(queryStr, queryArgs));
 
-return Promise.all(promiseArray).then((results) => {
-   const queryPromise = results[0]
+  return Promise.all(promiseArray).then((results) => {
+    const queryPromise = results[0];
 
-   return queryPromise.rows
-})
-
-}
-
-
+    return queryPromise.rows;
+  });
+};
 
 exports.selectCommentsByArticleId = (article_id) => {
-   return db
-   .query(`select comment_id, article_id, body, votes, author,
+  return db
+    .query(
+      `select comment_id, article_id, body, votes, author,
        created_at from comments 
        where article_id = $1
-       order by created_at desc; `, [article_id])
-       .then(( result ) => {
-         return result;
-       })
-}
+       order by created_at desc; `,
+      [article_id]
+    )
+    .then((result) => {
+      return result;
+    });
+};
 
 exports.addCommentForArticle = (article_id, username, body) => {
-   
-   return db
-   .query(`insert into comments (article_id, author, body)
+  return db
+    .query(
+      `insert into comments (article_id, author, body)
       values
       ($1, $2, $3) returning *;`,
-   [article_id, username, body]).then((result) => {
-      return result.rows[0]
+      [article_id, username, body]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
+};
 
-
-   })
-}
-
-exports.changeVotesForArticle = (article_id, newVote ) => {
-
-   return db
-   .query(`update articles 
+exports.changeVotesForArticle = (article_id, newVote) => {
+  return db
+    .query(
+      `update articles 
       set votes = greatest(votes + $1, 0)
       where article_id = $2
       returning *;`,
-   [newVote, article_id]).then((result) => {
-      return result.rows[0]
-   })
-}
+      [newVote, article_id]
+    )
+    .then((result) => {
+      return result.rows[0];
+    });
+};
 
 exports.deleteComment = (comment_id) => {
-   return db
-   .query(`delete from comments
+  return db.query(
+    `delete from comments
       where comment_id = $1
-      returning *;`, [comment_id])
-}
+      returning *;`,
+    [comment_id]
+  );
+};
 
 exports.selectUsers = () => {
-   return db
-   .query(`select * from users;`)
-}
+  return db.query(`select * from users;`);
+};
